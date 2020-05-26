ed7c7060a6fdb2550f259f1a780d6aeb0643de8b
Shambhu Gohel <gohelshambhu95@yahoo.com>
2020-05-26 14:18:36 +0530

    Problem 21 Added

---
+++ HackerRankPractice/src/com/sam/arrayProblem/ArrayPart2Sol.java
@@ -0,0 +1,44 @@
+package com.sam.arrayProblem;
+
+import java.util.Scanner;
+
+public class ArrayPart2Sol {
+
+	public static boolean canWin(int leap, int[] game) {
+		if (game == null) {
+			return false;
+		}
+		return isSolvable(leap, game, 0);
+	}
+
+	private static boolean isSolvable(int leap, int[] game, int i) {
+		// Base Cases
+		if (i >= game.length) {
+			return true;
+		} else if (i < 0 || game[i] == 1) {
+			return false;
+		}
+
+		game[i] = 1;
+
+		return isSolvable(leap, game, i + leap) || isSolvable(leap, game, i + 1) || isSolvable(leap, game, i - 1);
+
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int q = scan.nextInt();
+		while (q-- > 0) {
+			int n = scan.nextInt();
+			int leap = scan.nextInt();
+
+			int[] game = new int[n];
+			for (int i = 0; i < n; i++) {
+				game[i] = scan.nextInt();
+			}
+
+			System.out.println((canWin(leap, game)) ? "YES" : "NO");
+		}
+		scan.close();
+	}
+}
